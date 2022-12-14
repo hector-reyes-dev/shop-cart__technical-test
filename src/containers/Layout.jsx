6c87dbaa872d485059b1f529881c0b6c2c2ec134
Hector <hectordevx@gmail.com>
2022-12-14 16:11:56 -0600

    feat(project): initial commit

---
+++ src/containers/Layout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <p>Layout Works</p>
+      <div className="Layout">{children}</div>;
+    </>
+  );
+};
+
+export default Layout;
