5e1fbe2cd612b231743c4721afd46c9f7ecb3ee1
Hector <hectordevx@gmail.com>
2022-12-15 20:57:08 -0600

    feat(Components): Hero Component added

---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Hero.scss";
+
+const Hero = () => {
+  return (
+    <section className="hero-container">
+      <img
+        src="https://chedrauimx.vteximg.com.br/arquivos/DS_Martimiercoles_Mayo2022.svg"
+        alt="Imagen Martimíercoles cuesta menos"
+      />
+    </section>
+  );
+};
+
+export default Hero;
